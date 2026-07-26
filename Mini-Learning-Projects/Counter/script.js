0fca96242ddd3577b9a563b946ca484ba3690ea5
pavani-123161 <pavanirdyv@gmail.com>
2026-07-26 16:17:15 +0530

    Created Counter Program using JS Basics

---
+++ Mini-Learning-Projects/Counter/script.js
@@ -0,0 +1,19 @@
+
+const countLabel = document.getElementById("countLabel");
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+let count = 0;
+
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+resetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
